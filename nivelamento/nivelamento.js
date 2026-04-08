8bbceb4b3797690753099e2fd930c2b4beb25c4b
gustavofewv <gustavo.oliveira.alcantara@escola.pr.gov.br>
2026-04-08 11:54:05 -0300

    Add files via upload

---
+++ nivelamento/nivelamento.js
@@ -0,0 +1,32 @@
+//1. exiba a "menagen ola mundo"
+console.log("ola mundo")
+
+// 2. crie uma variavel com seu nome e exiba seu nome 
+let nome = "gustavo"
+console.log(nome);
+
+// 3.some dois numeros e exiba o resultado
+
+let soma = 5 + 3;
+console.log(soma);
+
+// 4.ubtraia dois numeros e exiba o resultado
+
+let soma2 = 5 - 3;
+console.log(soma2);
+
+
+//descubra o resto a ivisao de um numero por outro
+
+let resto = 5 * 3;
+console.log(resto);
+
+//crie uma variavel boleana e exiba seu valor
+
+let estachovendo =true;
+console.log(etachovendo);
+
+//verifique se um numero e maior que o outro e exiba o resultado
+  
+let maiorque=18 > 22;
+console.log(maiorque);
